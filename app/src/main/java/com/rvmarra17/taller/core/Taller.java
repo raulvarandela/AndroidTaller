e8b54263f44374d8f2af0dcf6013d43a50090410
Raul Varandela <raulvarandela@protonmail.com>
2020-10-19 12:22:14 +0200

    Frist Commit

---
+++ app/src/main/java/com/rvmarra17/taller/core/Taller.java
@@ -0,0 +1,57 @@
+package com.rvmarra17.taller.core;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Taller {
+    public static final String[] SERVICIOS = new String[]{
+            "Revison motor",
+            "Cambio de aceite",
+            "Equelibrado de ruedas"
+    };
+
+    public static final double[] COSTES = new double[]{
+            9.99,
+            29.99,
+            4.99
+    };
+
+    public Taller() {
+        this.serviosContratados = new boolean[SERVICIOS.length];
+        this.serviosContratados[0] = true;
+
+    }
+
+    public double getTotalServicios() {
+        double toret = 0f;
+
+        for (int i = 0; i < COSTES.length; i++) {
+            if (this.serviosContratados[i]) toret += COSTES[i];
+        }
+        return toret;
+    }
+
+    public void contrataServicio(int i, boolean value) {
+        serviosContratados[i] = value;
+    }
+
+    private boolean[] serviosContratados;
+
+    public String[] getServiciosAsString() {
+        ArrayList<String> toret = new ArrayList<>();
+
+        for (int i = 0; i < SERVICIOS.length; i++) {
+            if (serviosContratados[i]) {
+                toret.add(SERVICIOS[i]);
+            }
+        }
+
+        return toret.toArray(new String[0]);
+    }
+
+    public boolean[] getServicios() {
+        //return serviosContratados;
+        return Arrays.copyOf(serviosContratados, serviosContratados.length);
+
+    }
+}
---
+++ app/src/main/java/com/rvmarra17/taller/ui/MainActivity.java
@@ -0,0 +1,88 @@
+package com.rvmarra17.taller.ui;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.rvmarra17.taller.R;
+import com.rvmarra17.taller.core.Taller;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //inicializar taller
+        taller = new Taller();
+
+        //inicializar las vistas
+        final Button inserta = this.findViewById(R.id.inserta);
+        inserta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertar();
+            }
+        });
+
+        this.actualiza();
+    }
+
+
+    private void actualiza() {
+        actualizarLista();
+        actualizaTotal();
+    }
+
+    private void actualizaTotal() {
+        final TextView total = this.findViewById(R.id.total);
+        total.setText(String.format("Total: %4.2f€", taller.getTotalServicios()));
+    }
+
+    private void actualizarLista() {
+        final ListView servicios = this.findViewById(R.id.servicos);
+        ArrayAdapter<String> adap = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                taller.getServiciosAsString()
+        );
+
+        servicios.setAdapter(adap);
+
+    }
+
+    private void insertar(){
+        final AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+        dlg.setMultiChoiceItems(
+                Taller.SERVICIOS,
+                this.taller.getServicios(),
+                new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        taller.contrataServicio(which, isChecked);
+                    }
+                }
+        );
+
+        dlg.setPositiveButton("Añadir",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        actualiza();
+                    }
+                });
+
+        dlg.create().show();
+    }
+
+
+    private Taller taller;
+}
